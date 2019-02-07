401bf1ded10a9d0c8483b1ba36d65d41f5594ad1
Björn Petersen <pheasn@gmail.com>
2019-02-07 22:48:46 +0100

    Try to modularize

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,11 @@ application {
     mainClassName = "net.bjoernpetersen.shutdown.Main"
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
 dependencies {
     // Basics
     implementation(kotlin("stdlib-jdk8", version = Lib.KOTLIN))
---
+++ src/main/java/module-info.java
@@ -0,0 +1,24 @@
+module ShutdownServer {
+    requires kotlin.stdlib;
+    requires kotlin.reflect;
+    requires kotlin.logging;
+    requires ch.qos.logback.core;
+    requires ch.qos.logback.classic;
+
+    requires cfg4k.core;
+    requires cfg4k.yaml;
+
+    requires ST4;
+
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.dataformat.yaml;
+    requires com.fasterxml.jackson.module.kotlin;
+
+    requires javax.inject;
+    requires dagger;
+
+    requires vertx.core;
+    requires vertx.web;
+    requires vertx.lang.kotlin;
+}
