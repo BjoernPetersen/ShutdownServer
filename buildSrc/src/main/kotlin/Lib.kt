d2afbdf0f01af158af0e2d8a4443016daa1e8b92
Björn Petersen <pheasn@gmail.com>
2019-05-21 19:57:08 +0200

    Update dependency versions

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,14 +1,14 @@
 object Lib {
     const val KOTLIN = Plugin.KOTLIN
-    const val CFG4K = "0.9.0"
-    const val KOTLIN_LOGGING = "1.6.23"
-    const val SLF4J = "1.8.0-beta2"
+    const val CFG4K = "0.9.41"
+    const val KOTLIN_LOGGING = "1.6.26"
+    const val SLF4J = "1.8.0-beta4"
     const val LOGBACK = "1.3.0-alpha4"
 
-    const val VERTX = "3.6.3"
+    const val VERTX = "3.7.0"
     const val DAGGER = Plugin.DAGGER
-    const val JACKSON = "2.9.8"
+    const val JACKSON = "2.9.9"
     const val STRING_TEMPLATE = "4.1"
 
-    const val JUNIT = "5.4.0"
+    const val JUNIT = "5.4.2"
 }
--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -1,9 +1,9 @@
 object Plugin {
-    const val VERSIONS = "0.20.0"
+    const val VERSIONS = "0.21.0"
 
-    const val KOTLIN = "1.3.21"
-    const val DOKKA = "0.9.17"
+    const val KOTLIN = "1.3.31"
+    const val DOKKA = "0.9.18"
     const val SHADOW_JAR = "5.0.0"
 
-    const val DAGGER = "2.21"
+    const val DAGGER = "2.22.1"
 }
