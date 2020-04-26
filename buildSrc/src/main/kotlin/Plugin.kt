d40f4d10108a2b0dbec6f5aeaaecf7ae02bad521
Björn Petersen <pheasn@gmail.com>
2020-04-26 12:37:56 +0200

    Update Kotlin version

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -2,7 +2,7 @@ object Plugin {
     const val VERSIONS = "0.28.0"
     const val SPOTLESS = "3.28.1"
 
-    const val KOTLIN = "1.3.71"
+    const val KOTLIN = "1.3.72"
     const val DOKKA = "0.10.1"
     const val SHADOW_JAR = "5.2.0"
 
